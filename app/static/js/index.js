a4ef2f922437abfe37bc5944d2afbb3e5707844f
Welberth <welbertholiveira250@gmail.com>
2024-10-29 09:44:37 -0300

    Adicionando tag form para os formularios de input do usuario, na tela de login e cadastro

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,10 +1,19 @@
 function logar() {
-    var email = document.getElementById('e-mail').value;
-    var senha = document.getElementById('senha').value;
+    // Obtém os valores dos campos de e-mail e senha
+    var email = document.getElementById('e-mail').value.trim();
+    var senha = document.getElementById('senha').value.trim();
 
-    if(email == 'admin' && senha == 'admin'){
-        alert('sucesso');
-        location.href = "pagina_principal.html";
+    // Validação dos campos de e-mail e senha
+    if (email === '' || senha === '') {
+        alert('Por favor, preencha todos os campos.');
+        return; // Sai da função se algum campo estiver vazio
+    }
+
+    // Verifica se o e-mail e senha são iguais aos valores esperados
+    if (email === 'admin@gmail.com' && senha === 'admin') {
+        alert('Login realizado com sucesso!');
+        // Redireciona para a página principal
+        window.location.href = "pagina_principal.html";
     } else {
         alert('Usuário ou senha incorretos!');
     }
