b6964859d8b83165115b0f9bd888eadf8f1973e7
Welberth <welbertholiveira250@gmail.com>
2024-11-18 21:46:18 -0300

    Adicionando apps ao django

---
+++ cuidapet/medicamentos_e_vacinas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MedicamentosEVacinasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'medicamentos_e_vacinas'
---
+++ cuidapet/pagina_principal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PaginaPrincipalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pagina_principal'
