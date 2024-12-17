6dfc040d3d356d531542645a65e199578381739a
Welberth <welbertholiveira250@gmail.com>
2024-12-17 17:33:06 -0300

    banco de dados temporario pelo js, dinamica na pagina meus pets

---
+++ cuidapet/static/js/dados.js
@@ -0,0 +1,22 @@
+// Simulação de banco de dados temporário para os testes das páginas web
+// Usuário cadastrado
+const usuario = {
+    id: 1,
+    nome: "João Silva",
+    email: "joao.silva@email.com",
+    senha: "123456"
+};
+  
+// Pet do usuário
+const pets = [
+{
+    id: 1,
+    nome: "Rex",
+    tipo: "Cachorro",
+    raca: "Labrador",
+    idade: "2 anos",
+    ultimaVacina: "2024-09-10",
+    ultimaTosa: "2024-09-01"
+}
+];
+  
