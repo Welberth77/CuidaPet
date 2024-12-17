a13b84e055f07ddd79d6e63a73fdc4036c7081cf
Welberth <welbertholiveira250@gmail.com>
2024-12-17 13:18:12 -0300

    removendo django do projeto

--- cuidapet/meus_pets/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MeusPetsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meus_pets'
--- cuidapet/pagina_principal/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required  # Exige que o usuário esteja logado
-def home(request):
-    return render(request, 'pagina_principal/pagina_principal.html')
