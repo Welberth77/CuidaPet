0e5d70d3fe487bd2f9c9e66478aecf1e874ec014
PauluCiesar <pauluciesar@gmail.com>
2025-03-10 14:37:51 -0300

    sistema de validação ded usuario

--- apiautenticacao/src/controllers/authcontroller.js
+++ apiautenticacao/src/controllers/authcontroller.js
@@ -5,6 +5,15 @@ const usermodel = require("../models/user");
 const router = express.Router();
 
 router.post("/registrar", async (req, res) => {
+  const { email } = req.body;
+
+  if (await usermodel.findOne({ email })) {
+    return res.status(400).json({
+      erro: true,
+      message: "Usuario já existe",
+    });
+  }
+
   const user = await usermodel.create(req.body);
 
   return res.json({
