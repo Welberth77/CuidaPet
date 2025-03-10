d44943fa2f2255ef5fcea0396911255ae852c7c0
PauluCiesar <pauluciesar@gmail.com>
2025-03-10 11:12:19 -0300

    ajustes para teste no postman

--- apiautenticacao/src/server.js
+++ apiautenticacao/src/server.js
@@ -4,6 +4,8 @@ const authcontroller = require("./controllers/authcontroller");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/auth", authcontroller);
 
 app.get("/", (req, res) => {
