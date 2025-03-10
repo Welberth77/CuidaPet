d3a9056de9f311fab0db47339f042e155a56da4a
PauluCiesar <pauluciesar@gmail.com>
2025-03-09 21:34:34 -0300

    ajuste na configuração do database

--- apiautenticacao/src/controllers/authcontroller.js
+++ apiautenticacao/src/controllers/authcontroller.js
@@ -1,15 +1,15 @@
 const express = require("express");
 
-const usermodel + require("../models/user");
+const usermodel = require("../models/user");
 
 const router = express.Router();
 
 router.post("/registrar", (req, res) => {
-    console.log(req.body);
-    return res.json({
-        error: false,
-        message:"Registrado com sucesso"
-    });
-})
+  console.log(req.body);
+  return res.json({
+    error: false,
+    message: "Registrado com sucesso",
+  });
+});
 
 module.exports = router;
--- apiautenticacao/src/database/index.js
+++ apiautenticacao/src/database/index.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://pauluciesar:paulu050224@apiauthmongo.nwgp7.mongodb.net/",
-  {},
-  (error) => {
-    if (error) {
-      console.log("Falha ao autenticar com mongobd");
-      console.log(error);
-      return;
-    }
-    console.log("Conexão com mongodb estável");
-  }
-);
-
-mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    "mongodb+srv://pauluciesar:paulu050224@apiauthmongo.nwgp7.mongodb.net/?retryWrites=true&w=majority&appName=APIauthmongo"
+  )
+  .then(() => {
+    console.log("Conexão com MongoDB estável");
+  })
+  .catch((error) => {
+    console.log("Falha ao autenticar com MongoDB");
+    console.log(error);
+  });
 
 module.exports = mongoose;
+
+mongoose.Promise = global.Promise;
