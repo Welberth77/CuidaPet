7b435619041d9211da25347d2eaf4c1f47c131cc
PauluCiesar <pauluciesar@gmail.com>
2025-03-10 11:11:06 -0300

    ajuste do authcontroller

--- apiautenticacao/src/controllers/authcontroller.js
+++ apiautenticacao/src/controllers/authcontroller.js
@@ -4,11 +4,13 @@ const usermodel = require("../models/user");
 
 const router = express.Router();
 
-router.post("/registrar", (req, res) => {
-  console.log(req.body);
+router.post("/registrar", async (req, res) => {
+  const user = await usermodel.create(req.body);
+
   return res.json({
     error: false,
     message: "Registrado com sucesso",
+    data: user,
   });
 });
 
