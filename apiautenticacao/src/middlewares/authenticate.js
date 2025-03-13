d46fce2d587e9da9a5a77155a80c2f4d95208298
PauluCiesar <pauluciesar@gmail.com>
2025-03-12 22:57:17 -0300

    verificar se existe o token

--- apiautenticacao/src/middlewares/authenticate.js
+++ apiautenticacao/src/middlewares/authenticate.js
@@ -2,6 +2,13 @@ module.exports = (req, res, next) => {
   console.log("middleware");
   const authheader = req.headers.authorization;
 
+  if (!authheader) {
+    return res.status(401).json({
+      error: true,
+      message: "Token não fornecido",
+    });
+  }
+
   console.log(authheader);
 
   next();
