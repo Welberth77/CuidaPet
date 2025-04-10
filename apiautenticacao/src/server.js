43531961065e8ff774717dc51c5dee889e6965ed
PauluCiesar <pauluciesar@gmail.com>
2025-04-10 10:50:51 -0300

    adição de novas rotas no server.js

--- apiautenticacao/src/server.js
+++ apiautenticacao/src/server.js
@@ -1,17 +1,19 @@
 const express = require("express");
-
+const cors = require("cors"); // Adicionando CORS para permitir requisições do frontend
 const authcontroller = require("./controllers/authcontroller");
-
+const usercontroller = require("./controllers/usercontroller"); // Novo controller para operações de usuário
 const admincontroller = require("./controllers/admincontroller");
-
 const authenticatemiddleware = require("./middlewares/authenticate");
 
 const app = express();
 
+// Configurações básicas
+app.use(cors());
 app.use(express.json());
 
+// Rotas
 app.use("/auth", authcontroller);
-
+app.use("/user", authenticatemiddleware, usercontroller); // Novas rotas para operações de usuário
 app.use("/admin", authenticatemiddleware, admincontroller);
 
 app.get("/", (req, res) => {
