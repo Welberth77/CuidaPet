8107ab9d6de670bb5142aecbd8d8fd3b8122f5ef
PauluCiesar <pauluciesar@gmail.com>
2025-05-04 20:42:50 -0300

    função para mostrar apenas veterinarios e petshops

--- cuidapet/static/js/mapa.js
+++ cuidapet/static/js/mapa.js
@@ -38,7 +38,7 @@ function mostrarMinhaLocalizacao() {
           map: map,
           title: "Você está aqui!",
           icon: {
-            url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+            url: "https://cdn-icons-png.flaticon.com/512/616/616408.png",
             scaledSize: new google.maps.Size(30, 30), // ajustar Tamanho
           },
         });
@@ -55,12 +55,61 @@ function mostrarMinhaLocalizacao() {
         map.setZoom(15);
       },
       () => {
-        alert("Não foi possível obter sua localizaCÃO.");
+        alert("Não foi possível obter sua localização.");
       }
     );
   } else {
     alert("Geolocalização não é suportada pelo seu navegador.");
   }
+}
+
+async function mostrarLocais() {
+  const { PlacesService } = await google.maps.importLibrary("places");
+
+  const service = new google.maps.places.PlacesService(map);
+  const tipos = ["veterinary_care", "pet_store"];
+
+  tipos.forEach((tipo) => {
+    const request = {
+      location: myLatlng,
+      radius: 5000,
+      type: tipo,
+    };
+
+    service.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        results.forEach((place) => {
+          criarMarcador(place, tipo);
+        });
+      }
+    });
+  });
+}
+
+function criarMarcador(place, tipo) {
+  const icons = {
+    veterinary_care:
+      "https://maps.google.com/mapfiles/kml/shapes/hospitals.png",
+    pet_store: "https://maps.google.com/mapfiles/kml/shapes/hospitals.png",
+  };
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+    icon: {
+      url: icons[tipo],
+      scaledSize: new google.maps.Size(25, 25), // ajustar Tamanho
+    },
+    title: place.name,
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+    content: `<strong>${place.name}</strong><br>${place.vicinity}`,
+  });
+
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
+  });
 }
 
 window.initMap = initMap;
