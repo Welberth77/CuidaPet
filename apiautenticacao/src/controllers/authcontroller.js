a7233eb3c6e5f2dd518633e2af060a582df22234
PauluCiesar <pauluciesar@gmail.com>
2025-05-05 10:07:30 -0300

    Atualização do authcontroller.js

--- apiautenticacao/src/controllers/authcontroller.js
+++ apiautenticacao/src/controllers/authcontroller.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 
 const authconfig = require("../config/auth.json");
 
-const usermodel = require("../models/user");
+const User = require("../models/user"); // Note que agora importamos User (maiúsculo)
 
 const router = express.Router();
 
@@ -26,59 +26,62 @@ const generatetoken = (user = {}) => {
 router.post("/registrar", async (req, res) => {
   const { email } = req.body;
 
-  if (await usermodel.findOne({ email })) {
-    return res.status(400).json({
-      erro: true,
-      message: "Usuario já existe",
+  try {
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      return res.status(400).json({
+        erro: true,
+        message: "Usuario já existe",
+      });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.json({
+      user,
+      token: generatetoken(user),
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Erro ao registrar usuário",
+      details: error.message,
     });
   }
-
-  const user = await usermodel.create(req.body);
-
-  user.password = undefined;
-
-  const token = jwt.sign(
-    {
-      id: user.id,
-      name: user.name,
-    },
-    authconfig.secret,
-    {
-      expiresIn: 86400,
-    }
-  );
-
-  return res.json({
-    user,
-    token: generatetoken(user),
-  });
 });
 
 router.post("/autenticar", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await usermodel.findOne({ email }).select("+password");
+  try {
+    const user = await User.scope("withPassword").findOne({ where: { email } });
 
-  if (!user) {
-    return res.status(400).json({
+    if (!user) {
+      return res.status(400).json({
+        error: true,
+        message: "Usuario não encontrado",
+      });
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(400).send({
+        error: true,
+        message: "Senha invalida",
+      });
+    }
+
+    return res.json({
+      user,
+      token: generatetoken(user),
+    });
+  } catch (error) {
+    return res.status(500).json({
       error: true,
-      message: "Usuario não encontrado",
+      message: "Erro ao autenticar usuário",
+      details: error.message,
     });
   }
-
-  if (!(await bcrypt.compare(password, user.password))) {
-    return res.status(400).send({
-      error: true,
-      message: "Senha invalida",
-    });
-  }
-
-  user.password = undefined;
-
-  return res.json({
-    user,
-    token: generatetoken(user),
-  });
 });
 
 module.exports = router;
