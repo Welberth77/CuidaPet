d72ea03105122ee8a27db02d60e56bfc2f39382a
PauluCiesar <pauluciesar@gmail.com>
2025-05-05 10:10:31 -0300

    Atualização do admincontroller.js

--- apiautenticacao/src/controllers/admincontroller.js
+++ apiautenticacao/src/controllers/admincontroller.js
@@ -1,10 +1,20 @@
 const express = require("express");
 
+const User = require("../models/user");
+
 const router = express.Router();
 
-router.get("/usuarios", (req, res) => {
-  console.log("controller");
-  return res.json({});
+router.get("/usuarios", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    return res.json(users);
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Erro ao buscar usuários",
+      details: error.message,
+    });
+  }
 });
 
 module.exports = router;
