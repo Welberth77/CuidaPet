fc71e92bbfd20ccaf92fe8f063151cf7490b9a65
Welberth <welbertholiveira250@gmail.com>
2025-05-09 10:28:52 -0300

    fix: colocando id nas labels da página de cadastrar pet

--- cuidapet/static/js/cadastrarPet.js
+++ cuidapet/static/js/cadastrarPet.js
@@ -7,3 +7,33 @@ const pesoPet = document.getElementById("pesoPet");
 const nascimentoPet = document.getElementById("nascimentoPet");
 const corPelagem = document.getElementById("corPelagem");
 
+// Submit do formulario
+formulario.addEventListener("submit", (event) => {
+    // So atualiza a página quando o botão for clicado
+    event.preventDefault();
+
+    // Chamando verificação
+    checkNomePet();
+});
+
+
+// Verificando nome do pet
+function checkNomePet() {
+    const nomPetValue = nomePet.value;
+
+    if (nomePet == "") {
+        errorInput(nomePet);
+    } else {
+        const formularioitem = nomePet.parentElement;
+        formularioitem.className = "input-item";
+    }
+}
+
+
+// Erro de input, deixa apenas a borda vermelha
+function errorInput(input) {
+    // Pega o elemento pai do input
+    const formularioItem = input.parentElement;
+
+    formularioItem.className = "input-item error";
+}
