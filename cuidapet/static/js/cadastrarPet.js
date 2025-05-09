82e9b30dda42ebcf270a59ee806dae7ed74763c3
Welberth <welbertholiveira250@gmail.com>
2025-05-09 10:08:58 -0300

    style: ajuste na div de botão na página de cadastrar Pet

---
+++ cuidapet/static/js/cadastrarPet.js
@@ -0,0 +1,9 @@
+// Validações para cadastrar pet
+// Recebendo ids
+const formulario = document.getElementById("interacao-usuario");
+const nomePet = document.getElementById("nomePet");
+const racaPet = document.getElementById("racaPet");
+const pesoPet = document.getElementById("pesoPet");
+const nascimentoPet = document.getElementById("nascimentoPet");
+const corPelagem = document.getElementById("corPelagem");
+
