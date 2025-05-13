f8064f194795d4ab446b4e98dbc95c5d9ed61c50
PauluCiesar <pauluciesar@gmail.com>
2025-05-13 15:11:08 -0300

    instalação do cors / back-end da parte de login e cadastro

--- apiautenticacao/src/controllers/admincontroller.js
+++ apiautenticacao/src/controllers/admincontroller.js
@@ -17,4 +17,29 @@ router.get("/usuarios", async (req, res) => {
   }
 });
 
+router.put("/usuarios/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+
+    const user = await User.scope("withPassword").findByPk(id);
+    if (!user)
+      return res.status(404).json({ message: "Usuário não encontrado" });
+
+    user.name = name;
+    user.email = email;
+    if (password) user.password = password;
+
+    await user.save();
+
+    return res.json({ user });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Erro ao atualizar usuário",
+      details: error.message,
+    });
+  }
+});
+
 module.exports = router;
