a91d5aa6d7339539c1bb44a4e5e8c9b270f5e8ea
Welberth <welbertholiveira250@gmail.com>
2025-05-19 10:47:57 -0300

    feat: validação de selecionar serviço e data do banho atual

--- cuidapet/static/js/registrarBanhoETosa.js
+++ cuidapet/static/js/registrarBanhoETosa.js
@@ -13,6 +13,8 @@ formulario.addEventListener("submit", (event) => {
 
     // Checando validações
     checkSelecionarPet();
+    checkSelecionarServico();
+    checkData();
 })
 
 
@@ -25,6 +27,48 @@ function checkSelecionarPet() {
     } else {
         // Voltando a classe para normal
         const formularioItem = selecionarPet.parentElement;
+        formularioItem.className = "interacao-item";
+    }
+}
+
+// Verificalção no select do serviço
+function checkSelecionarServico() {
+    const selecionarServicoValue = selecionarServico.value;
+
+    if (selecionarServicoValue === "") {
+        errorInput(selecionarServico);
+    } else {
+        // Voltando a classe para a forma normal
+        const formularioItem = selecionarServico.parentElement;
+        formularioItem.className = "interacao-item";
+    }
+}
+
+// Verificação da data do banho e tosa
+function checkData() {
+    // Recebendo valor no tipo string
+    const dataNovoBanhoValue = dataNovoBanho.value;
+
+    // Verificando se está vazio
+    if (dataNovoBanhoValue === "") {
+        errorInput(dataNovoBanho);
+        return
+    }
+
+    // Tranformando no formato de data
+    const dataNovoBanhoValueData = new Date(dataNovoBanho.value);
+    // Data atual do usuário
+    const dataAtual = new Date();
+
+    // Descosiderar horas e minutos 
+    dataAtual.setHours(0, 0, 0, 0);
+
+    // Verificando se a data é maio que a atual
+    if (dataNovoBanhoValueData > dataAtual) {
+        errorInput(dataNovoBanho);
+    } else {
+        // Voltando a classe para a forma normal
+        const formularioItem = dataNovoBanho.parentElement;
         formularioItem.className = "interacao-item";
     }
 }
